1061e995e5d0554e4258929b94a2767e546ce198
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-08-01 19:26:05 -0400

    Created an example program

---
+++ EncapsulationExample_1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample_1
+{
+    public class Person
+    {
+        private double _wallet = 0; //fields are typically named with an _ infront
+        public string Name { get; set; } //properties are just fields with the getter and setter
+        public int Age { get; set; }
+
+        public void AddMoney(double moneyToAdd) //Assigning _wallet to this method to actually access this field and adding value to it
+        {
+            _wallet += moneyToAdd;
+        }
+
+        public double CheckWallet()
+        {
+            return _wallet;
+        }
+    }
+}
---
+++ EncapsulationExample_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Encapsulation is information hiding/secured
+using EncapsulationExample_1;
+
+var erik = new Person();
+var myWallet = erik.CheckWallet();
+
+Console.WriteLine("Money in wallet:");
+Console.WriteLine(myWallet);
+
+Console.WriteLine("Adding money...");
+
+
+erik.AddMoney(1000.00);
+myWallet = erik.CheckWallet();
+
+Console.WriteLine(myWallet);
+var str = "string";
+ 
+
